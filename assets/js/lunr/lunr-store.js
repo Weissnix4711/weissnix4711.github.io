48e876f6f0245f84908a8f53fe7e29413b014c15
Weissnix4711 <Weissnix4711@users.noreply.github.com>
2020-12-23 20:40:30 +0000

    jekyll build from Action f5aef4ed8f597c43c65d54bbfdc1605060f2cece

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "https://weissnix4711.github.io/jekyll/update/2020/10/05/welcome-to-jekyll/",
+        "teaser": null
+      },{
+        "title": "Hacking Sonoff T4EU1C with ESPHOME",
+        "excerpt":"What’s this? A no-neutral WiFi-controlled smart switch made by Sonoff? Yep, that’s right. Just like the previous TX models (Touch, T1, new-T1, T2, and T3), the T4 can be wirelessly controlled with the eWeLink app, or the touch sensor, and has a small LED to help you locate the switch...","categories": ["smart-home"],
+        "tags": ["Sonoff","t4","t4eu1c","ESPHome","home-assistant","hass.io"],
+        "url": "https://weissnix4711.github.io/smart-home/2020/12/13/sonoff-t4eu1c/",
+        "teaser": null
+      }]
