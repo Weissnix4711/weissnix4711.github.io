918338347cc3005218b532aea569f2e3f8a1b486
Weissnix4711 <Weissnix4711@users.noreply.github.com>
2024-05-25 15:19:21 +0000

    jekyll build from Action 6449aa74f4000933dde3837cc179e18cd9c635d5

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,31 @@
+var store = [{
+        "title": "Hacking Sonoff T4EU1C with ESPHOME",
+        "excerpt":"What’s this? A no-neutral WiFi-controlled smart switch made by Sonoff? Yep, that’s right. Just like the previous TX models (Touch, T1, new-T1, T2, and T3), the T4 can be wirelessly controlled with the eWeLink app, or the touch sensor, and has a small LED to help you locate the switch...","categories": ["smart-home"],
+        "tags": ["Sonoff","t4","t4eu1c","ESPHome","Home Assistant","hass.io"],
+        "url": "/smart-home/2020/12/13/sonoff-t4eu1c/",
+        "teaser": null
+      },{
+        "title": "My Home Assistant CCTV camera setup",
+        "excerpt":"ESPHome setup First, we have to configure and install ESPHome on the camera. Most ESP32-CAM modules do not have a USB-UART bridge onboard, so you will need an external adaptor. Those based on the CH340G chip are cheap and readily available from eBay, Aliexpress, etc, and perfectly suitable for flashing...","categories": ["smart-home"],
+        "tags": ["Smart Home","ESPHome","Home Assistant","hass.io"],
+        "url": "/smart-home/2022/08/30/hass-cctv/",
+        "teaser": null
+      },{
+        "title": "DNSCrypt + Dnsmasq setup with Waydroid",
+        "excerpt":"Preamble I’ll skip the usual shite about why you really should be using one of DNSCrypt, DoT or DoH nowadays. Okay, maybe not that last one if it can be avoided. So, here’s my very quick guide / reference to getting DNSCrypt and dnsmasq working. This has been tested with...","categories": ["linux-mobile"],
+        "tags": ["PinePhone","PPPro","Linux Mobile","DNS"],
+        "url": "/linux-mobile/2023/03/06/dnscrypt/",
+        "teaser": null
+      },{
+        "title": "PinePhone (Pro) Battery Charger Teardown",
+        "excerpt":"PinePhone Battery Charger I recently picked up this little battery charger made specifically for the Samsung J7 battery used by the PinePhone (Pro). It features: A single USB-C port Red/green status LEDs Yeah that’s about it. Very simple, but that’s good. Seems to work just fine, but for shits and...","categories": ["teardown"],
+        "tags": ["PinePhone","PPPro","Linux Mobile","Teardown"],
+        "url": "/teardown/2023/12/27/pinephone-battery-charger/",
+        "teaser": null
+      },{
+        "title": "Dell PERC H310 raid card flashed to IT Mode",
+        "excerpt":"SAS HBA (Host Bus Adapter) cards can be expensive, RAID cards are much cheaper. How does that make sense? They’re electronically identical? Well I can’t answer that, but I can tell you how to reflash your PERC H310 (and similar) cards to the firmware of the LSI 9211-8i card, an...","categories": ["homelab"],
+        "tags": ["Homelab","RAID","NAS"],
+        "url": "/homelab/2024/05/14/dell-h310-it-mode/",
+        "teaser": null
+      }]
